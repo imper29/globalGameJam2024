868cda27c137bd4683b43c03cd658561a05fc4ae
Amirali <amiralit2942@yahoo.com>
2024-01-24 22:40:50 -0400

    Picking Up Organs

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Marten;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
@@ -11,11 +10,12 @@ public class Character : MonoBehaviour
     [SerializeField] private GameObject obj;
     [SerializeField] private Animator animator;
     [SerializeField] private float speed;
-    [SerializeField] private Transform[] organHolder;
+    [SerializeField] private GameObject[] organHolder;
     [SerializeField] private float pickingRange;
     [SerializeField] private LayerMask organLayer;
+    [SerializeField] private GameObject testObj;
     private int _MessedUp;
-
+    private bool _hasAnOrgan;
 
     void Update()
     {
@@ -52,23 +52,38 @@ public class Character : MonoBehaviour
         Debug.DrawLine(new Vector3(transform.position.x, transform.position.y - 2.9f, 0),
             new Vector2(transform.position.x, transform.position.y - 2.9f + pickingRange));
     }
-    
+
     private void PickUpOrgan()
     {
         var charPos = transform.position;
-        var ray = Physics2D.BoxCastAll(new Vector2(charPos.x, charPos.y/* - 2.9f*/), new Vector2(2, pickingRange), 0,
+        var ray = Physics2D.BoxCastAll(new Vector2(charPos.x, charPos.y /* - 2.9f*/), new Vector2(2, pickingRange), 0,
             Vector2.down);
         foreach (var hit in ray)
         {
             if (hit.transform.gameObject.GetComponent<Organ>() != null)
             {
-                if (Vector2.Distance(transform.position,hit.transform.position) <= pickingRange)
+                if (Vector2.Distance(transform.position, hit.transform.position) <= pickingRange)
                 {
+                    if (hit.transform.gameObject.GetComponent<Organ>().isInPlayerHands)
+                        continue;
+                    
+                    if (_hasAnOrgan)
+                    {
+                        //Swap Organs
+                        organHolder[_MessedUp].transform.GetChild(0).transform.position = hit.transform.position;
+                        organHolder[_MessedUp].transform.GetChild(0).GetComponent<Organ>().isInPlayerHands = false;
+                        // organHolder[_MessedUp].GetComponentInChildren<Organ>().transform.position =
+                        //     hit.transform.position;
+                        organHolder[_MessedUp].GetComponentInChildren<Organ>().transform.parent = null;
+                    }
+                    // Pick an Organ
                     print(hit.transform.gameObject.name);
-                    hit.transform.parent = organHolder[_MessedUp];
-                    print("parent" + hit.transform.parent);    
+                    hit.transform.parent = organHolder[_MessedUp].transform;
+                    hit.transform.localPosition = new Vector3();
+                    hit.transform.gameObject.GetComponent<Organ>().isInPlayerHands = true;
+                    _hasAnOrgan = true;
+                    return;
                 }
-                
             }
         }
     }
--- Assets/Scripts/Organ.cs
+++ Assets/Scripts/Organ.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Organ : MonoBehaviour
 {
     public OrganType Type => m_Type;
-
+    public bool isInPlayerHands;
     [SerializeField]
     private OrganType m_Type;
 }
