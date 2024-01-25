4d4b2e823a597c4d8d02a8772a7a3c4da8c6c60c
Amirali <amiralit2942@yahoo.com>
2024-01-24 23:15:30 -0400

    Implanting Organs

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Character : MonoBehaviour
@@ -12,10 +7,9 @@ public class Character : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private GameObject[] organHolder;
     [SerializeField] private float pickingRange;
-    [SerializeField] private LayerMask organLayer;
-    [SerializeField] private GameObject testObj;
-    private int _MessedUp;
+    private int _messedUp;
     private bool _hasAnOrgan;
+    private Organ _carryingOrgan;
 
     void Update()
     {
@@ -46,11 +40,14 @@ public class Character : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             PickUpOrgan();
+            TransplantOrgan();
         }
 
         //Debug
         Debug.DrawLine(new Vector3(transform.position.x, transform.position.y - 2.9f, 0),
             new Vector2(transform.position.x, transform.position.y - 2.9f + pickingRange));
+        Debug.DrawLine(new Vector3(transform.position.x, transform.position.y + 2.9f, 0),
+            new Vector2(transform.position.x, transform.position.y + 2.9f + pickingRange));
     }
 
     private void PickUpOrgan()
@@ -60,38 +57,67 @@ public class Character : MonoBehaviour
             Vector2.down);
         foreach (var hit in ray)
         {
-            if (hit.transform.gameObject.GetComponent<Organ>() != null)
+            if (hit.transform.gameObject.GetComponent<Organ>() == null)
+                return;
+            if (Vector2.Distance(transform.position, hit.transform.position) >= pickingRange)
+                return;
+            if (hit.transform.gameObject.GetComponent<Organ>().isInPlayerHands)
+                continue;
+
+            if (_hasAnOrgan)
             {
-                if (Vector2.Distance(transform.position, hit.transform.position) <= pickingRange)
-                {
-                    if (hit.transform.gameObject.GetComponent<Organ>().isInPlayerHands)
-                        continue;
-                    
-                    if (_hasAnOrgan)
-                    {
-                        //Swap Organs
-                        organHolder[_MessedUp].transform.GetChild(0).transform.position = hit.transform.position;
-                        organHolder[_MessedUp].transform.GetChild(0).GetComponent<Organ>().isInPlayerHands = false;
-                        // organHolder[_MessedUp].GetComponentInChildren<Organ>().transform.position =
-                        //     hit.transform.position;
-                        organHolder[_MessedUp].GetComponentInChildren<Organ>().transform.parent = null;
-                    }
-                    // Pick an Organ
-                    print(hit.transform.gameObject.name);
-                    hit.transform.parent = organHolder[_MessedUp].transform;
-                    hit.transform.localPosition = new Vector3();
-                    hit.transform.gameObject.GetComponent<Organ>().isInPlayerHands = true;
-                    _hasAnOrgan = true;
-                    return;
-                }
+                //Swap Organs
+                organHolder[_messedUp].transform.GetChild(0).transform.position = hit.transform.position;
+                organHolder[_messedUp].transform.GetChild(0).GetComponent<Organ>().isInPlayerHands = false;
+                organHolder[_messedUp].GetComponentInChildren<Organ>().transform.parent = null;
             }
+
+            // Pick an Organ
+            print(hit.transform.gameObject.name);
+            hit.transform.parent = organHolder[_messedUp].transform;
+            hit.transform.localPosition = new Vector3();
+            hit.transform.gameObject.GetComponent<Organ>().isInPlayerHands = true;
+            _hasAnOrgan = true;
+            return;
+        }
+    }
+
+
+    private void TransplantOrgan()
+    {
+        if (!_hasAnOrgan)
+            return;
+        var charPos = transform.position;
+        var ray = Physics2D.BoxCastAll(new Vector2(charPos.x, charPos.y + 2.9f), new Vector2(2, pickingRange), 0,
+            Vector2.up);
+        foreach (var hit in ray)
+        {
+            if (hit.transform.gameObject.GetComponent<Customer>() == null)
+            {
+                print("Null");
+                return;
+            }
+
+
+            if (Vector2.Distance(transform.position, hit.transform.position) >= pickingRange)
+            {
+                print("Dist: " + Vector2.Distance(transform.position, hit.transform.position));
+                print("Range: " + pickingRange);
+                return;
+            }
+
+
+            hit.transform.gameObject.GetComponent<Customer>()
+                .ImplantOrgan(organHolder[_messedUp].transform.GetChild(0).GetComponent<Organ>());
+            Destroy(organHolder[_messedUp].transform.GetChild(0).GetComponent<Organ>().gameObject);
+            _hasAnOrgan = false;
         }
     }
 
     private void PlayerMessedUp()
     {
-        _MessedUp++;
-        if (_MessedUp >= 3)
+        _messedUp++;
+        if (_messedUp >= 3)
         {
             //GameOver
         }
