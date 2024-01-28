0caa283acd1d039add81c9b794ca5f7a586d42c0
imper29 <38094274+imper29@users.noreply.github.com>
2024-01-28 15:50:32 -0500

    Updated animations, did belt animations, and imported sound effects.

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -8,6 +8,10 @@ public class ConveyorBelt : MonoBehaviour
     private float m_SpeedMax;
     [SerializeField]
     private Vector2 m_Direction;
+    [SerializeField]
+    private float m_BeltRendererWidth;
+    [SerializeField]
+    Transform m_BeltRenderer;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -16,9 +20,16 @@ public class ConveyorBelt : MonoBehaviour
         if (body != null)
             body.position += speed * Time.fixedDeltaTime * m_Direction;
     }
+    private void Update()
+    {
+        m_BeltRendererPos += Time.deltaTime * GetSpeed();
+        m_BeltRenderer.transform.localPosition = m_Direction * (m_BeltRendererPos % m_BeltRendererWidth);
+    }
 
     float GetSpeed()
     {
         return Mathf.Lerp(m_SpeedMin, m_SpeedMax, GameManager.Instance.GetDifficulty());
     }
+
+    private float m_BeltRendererPos;
 }
